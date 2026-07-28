9f90ef7c0c3a5021c80aa5f98dac9e1ff63fca05
torgrim <147815453+torgrimtk@users.noreply.github.com>
2026-07-28 17:16:05 +0200

    added a comment

--- TheWeepingTankard/src/App.tsx
+++ TheWeepingTankard/src/App.tsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+// Just a line of code, dont worry about it
 function App() {
   const [count, setCount] = useState(0)
 
